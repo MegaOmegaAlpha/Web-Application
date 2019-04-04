40e7fa12172ea40079f876e3c7cdd0ea8dea2edc
vladimir <bajjramov.vladimir@gmail.com>
2019-04-05 00:53:40 +0400

    подготовка к слиянию

--- src/main/java/modelForTraining/ArtistRepository.java
+++
@@ -1,9 +0,0 @@
-package modelForTraining;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArtistRepository extends CrudRepository<Artist, Integer> {
-
-    Artist findByName(String name);
-
-}
--- src/main/java/modelForTraining/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package modelForTraining;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface GenreRepository extends CrudRepository<Genre, Integer> {
-
-    Genre findByName(String name);
-
-}
